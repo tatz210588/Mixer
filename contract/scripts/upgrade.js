9d5128c466210ec02f020b2aec7290d212bc1e4d
netanmangal <imnetanmangal@gmail.com>
2023-05-10 23:25:08 +0530

    Updated artifacts pushed

--- contract/scripts/upgrade.js
+++ contract/scripts/upgrade.js
@@ -2,7 +2,7 @@ const { ethers, upgrades } = require("hardhat")
 
 async function main() {
     const mixer = await ethers.getContractFactory("Mixer");
-    let proxy = await upgrades.upgradeProxy("0x15BA1eaB00e5E130d142B6B364357251566c1999", mixer); //bsc-testnet
+    let proxy = await upgrades.upgradeProxy("0xb7Bc26b77D846FbB7F46BD59703F746B7073CB6E", mixer); //bsc-testnet
     console.log("Mixer Contract has been successfully upgraded...")
 }
 
